11a0ef074b77f3ce9475bdff42ec0dc7a9d9799b
Iosub Ovidiu-Marian <63242787+MarianIosub@users.noreply.github.com>
2021-04-19 14:17:10 +0300

    Added word-controller function and roumanian-words csv file.

---
+++ src/main/java/WordGamesMain.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class WordGamesMain {
+    public static void main(String[] args) throws IOException {
+        //System.out.println(WordController.verifyWordExistence("rdafasfa")?"Cuvantul a fost gasit!":"Cuvantul nu a fost gasit!");
+        //System.out.println(WordController.extractEasyWord());
+        //System.out.println(WordController.extractMediumWord());
+        //System.out.println(WordController.extractHardWord());
+        //System.out.println(WordController.verifyTheDiacritics("Marioara","Mărioara"));
+        //System.out.println(WordController.fazanRule("Mirela","lautari"));
+    }
+}
